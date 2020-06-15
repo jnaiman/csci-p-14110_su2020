b46fa2e455967150f26789fcd3a678b31bf07307
Jill Naiman <jnaiman@gmail.com>
2020-06-15 17:21:44 -0500

    lesson 06

--- _site/lesson06/image_manipulations.py
+++ _site/lesson06/image_manipulations.py
@@ -28,3 +28,43 @@ def my_boolean_mask(im_data, rgba):
     #mask = np.repeat(mask,im_data.shape[2],axis=2)
 
     return pixel_mask_good
+
+def color_components(im_data):
+    number_of_pixels_of_a_color = []
+    color_labels = []
+    color_rgb_labels = []
+    colors = []
+    unique_colors = np.unique(im_data.reshape(-1, im_data.shape[2]), axis=0)
+    for icolor,rgba in enumerate(unique_colors):    
+        # mask each channel
+        reds_mask = im_data[:,:,0] == rgba[0]
+        greens_mask = im_data[:,:,1] == rgba[1]
+        blues_mask = im_data[:,:,2] == rgba[2]
+        if len(im_data[0,0,:]) > 3: # we have an alphas channel and we should deal with that!
+            alphas_mask = im_data[:,:,3] == rgba[3]
+            # combined mask
+            pixel_mask = reds_mask & greens_mask & blues_mask & alphas_mask
+        else:
+            # combined mask
+            pixel_mask = reds_mask & greens_mask & blues_mask
+
+
+        # grab number of pixels
+        this_color_pixels = im_data[pixel_mask]
+        number_of_pixels_of_a_color.append(len(this_color_pixels))
+        # this could be done better...
+        color_labels.append( 'Color #' + str(icolor) )
+        
+        colorLabRGB = ''
+        for c in rgba:
+            colorLabRGB += str(c) + ','
+        colorLabRGB = colorLabRGB[:-1]
+        
+        color_rgb_labels.append(colorLabRGB)
+
+        colors.append( rgba/255 )
+
+        # cleaning at the end of the loop
+        del pixel_mask; del reds_mask; del greens_mask; del blues_mask
+        
+    return colors, color_labels, color_rgb_labels, number_of_pixels_of_a_color
